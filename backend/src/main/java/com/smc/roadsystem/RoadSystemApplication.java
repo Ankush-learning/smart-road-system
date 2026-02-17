07860de8d6e37f5d9c1059f11726d66c256fad74
Ankush-learning <ankushkyt1221@gmail.com>
2026-02-17 13:00:51 +0530

    Initial full stack commit

---
+++ backend/src/main/java/com/smc/roadsystem/RoadSystemApplication.java
@@ -0,0 +1,9 @@
+package com.smc.roadsystem;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class RoadSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RoadSystemApplication.class, args);
+    }
+}
