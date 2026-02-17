9033491a314739a2a96d9eea960c1a6bb4504037
Ankush-learning <ankushkyt1221@gmail.com>
2026-02-17 21:37:28 +0530

    Fix main render and test screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function Home() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div>Landing Page</div>} />
-      </Routes>
-    </BrowserRouter>
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "32px",
+      fontWeight: "bold"
+    }}>
+      SMART ROAD SYSTEM LIVE
+    </div>
   );
 }
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
 export default App;
