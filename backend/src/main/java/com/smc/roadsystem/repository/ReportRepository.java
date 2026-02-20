025641466bceb214fc66a48637fdcc25a073e6a7
Ankush-learning <ankushkyt1221@gmail.com>
2026-02-20 17:50:16 +0530

    Create ReportRepository.java

---
+++ backend/src/main/java/com/smc/roadsystem/repository/ReportRepository.java
@@ -0,0 +1,9 @@
+package com.smc.roadsystem.repository;
+
+import com.smc.roadsystem.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+    List<Report> findByUserIdOrderByCreatedAtDesc(Long userId);
+}
