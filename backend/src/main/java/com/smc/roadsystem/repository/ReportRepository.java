e409b777859fb70f2fc61a26d55fd9e26ba0062d
Ankush-learning <ankushkyt1221@gmail.com>
2026-02-21 06:24:28 +0530

    Update ReportRepository.java

--- backend/src/main/java/com/smc/roadsystem/repository/ReportRepository.java
+++ backend/src/main/java/com/smc/roadsystem/repository/ReportRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
     List<Report> findByUserIdOrderByCreatedAtDesc(Long userId);
+    List<Report> findAllByOrderByCreatedAtDesc();
 }
